b34a3790414c638ab4727c8ff846072a8581087b
AnaCarolinaMoraesBelo <moraesbeloa@gmail.com>
2025-07-23 09:36:03 -0300

    arquivo novo

---
+++ objetos/escola/AlunoAula23_07_Carol.js
@@ -0,0 +1,8 @@
+class AlunoAula23_07 {  //cri a classe
+    matricula;
+  }
+  
+  matheus = new AlunoAula23_07();
+  matheus.matricula = 23202454;  // atribui o valor da variável matricula
+  console.log(matheus.matricula);  //imprime na tela o valor da variável matrícula
+  
